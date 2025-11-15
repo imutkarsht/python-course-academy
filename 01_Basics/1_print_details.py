a037ee40e7443f8920f6d16b5ec2d590e7dce013
Utkarsh Tiwari <uktiwari023@gmail.com>
2025-11-15 18:28:59 +0530

    solve 1 basic

--- 01_Basics/1_print_details.py
+++ 01_Basics/1_print_details.py
@@ -1,3 +1,6 @@
 # Ask user for name, age, and city
-
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
 # Print the details in one sentence
+print(f"My name is {name}, I am {age} years old, and I live in {city}.")
